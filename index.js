6064a2a79a016e75755df361075fdaf32f5cebc9
Mr.YanG <yangyonghai9125@hotmail.com>
2019-11-26 11:52:16 +0800

    feat: rock scissor paper

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-console.log(process.argv);
 
+var userAction = process.argv[process.argv.length - 1];
+
+console.log('userAction ' +  userAction);
+var computerAction;
+var random = Math.random() * 3;
+
+if (random < 1) {
+  computerAction = 'rock';
+} else if (random > 2) {
+  computerAction = 'scissor';
+} else {
+  computerAction = 'paper';
+}
+console.log('computerAction ' + computerAction);
+
+if (userAction === computerAction) {
+  console.log('tie');
+} else if (
+  (computerAction === 'rock' && userAction === 'paper') || 
+  (computerAction === 'paper' && userAction === 'scissor') ||
+  (computerAction === 'scissor' && userAction === 'rock')
+) {
+  console.log('you win!');
+} else {
+  console.log('you lose');
+}

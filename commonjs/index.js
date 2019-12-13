7c9f35193437f1e7c6c49e4fbccdb218d367f37b
Mr.YanG <yangyonghai9125@hotmail.com>
2019-12-13 14:31:43 +0800

    feat: update games with commonjs module

--- commonjs/index.js
+++ commonjs/index.js
@@ -1,11 +1,18 @@
-// console.log('start');
-// 通过require() 引入的js文件，先执行，然后会返回一个exports对象
-/**
- * 通过require引入('./lib')，发生的情况
- * 1. 创建一个空的module对象
- * 
- */
-var lib = require('./lib');
-console.log(typeof lib);
-lib.asyc = 'asyc';
-console.log(lib);
+
+const game = require('./lib');
+
+// var userAction = process.argv[process.argv.length - 1];
+// let result = game(userAction);
+
+let winCount = 0;
+process.stdin.on('data', (buffer) => {
+  const userAction = buffer.toString().trim();
+  const result = game(userAction);
+  if (result == 1) {
+    winCount++;
+    if (winCount == 3) {
+      console.log('我不玩了，你太牛逼了!');
+      process.exit();
+    }
+  }
+})  
